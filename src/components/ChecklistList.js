16dc137d65b04fc8f1fd634807a66b7e569ee250
Andrey Sitnikov <a.v.sitnikov@gmail.com>
2024-08-25 11:51:42 +0300

    Изменение заголовков страниц

--- src/components/ChecklistList.js
+++ src/components/ChecklistList.js
@@ -1,60 +1,90 @@
-// src/components/ChecklistList.js
-import React, { useEffect, useState } from 'react';
-import { List, ListItem, ListItemText, Typography, Container, CircularProgress } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { setChecklistId } from '../store';
-import { getChecklists } from '../api';
+import React, { useEffect, useState } from "react";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Container,
+  CircularProgress,
+  IconButton,
+  Box,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import HomeIcon from "@mui/icons-material/Home";
+import { setChecklistId } from "../store";
+import { getChecklists } from "../api";
 
 const ChecklistList = () => {
-    const [checklists, setChecklists] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const { chatId, token, objectId } = useSelector((state) => state.app);
+  const [checklists, setChecklists] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { chatId, token, objectId } = useSelector((state) => state.app);
 
-    useEffect(() => {
-        const fetchChecklists = async () => {
-            try {
-                const data = await getChecklists(token, chatId, objectId);
-                setChecklists(data);
-                setLoading(false);
-            } catch (error) {
-                console.error("Error fetching checklists:", error);
-                setLoading(false);
-            }
-        };
-
-        fetchChecklists();
-    }, [objectId, chatId, token]);
-
-    const handleSelectChecklist = (checklistId) => {
-        dispatch(setChecklistId(checklistId));
-        navigate(`/questions`);
+  useEffect(() => {
+    const fetchChecklists = async () => {
+      try {
+        const data = await getChecklists(token, chatId, objectId);
+        setChecklists(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching checklists:", error);
+        setLoading(false);
+      }
     };
 
-    if (loading) {
-        return <CircularProgress />;
-    }
+    fetchChecklists();
+  }, [objectId, chatId, token]);
 
-    return (
-        <Container>
-            <Typography variant="h4" gutterBottom>
-                Checklists
-            </Typography>
-            <List>
-                {checklists.map((checklist, index) => (
-                    <ListItem button key={index} onClick={() => handleSelectChecklist(checklist.model_id)}>
-                        <ListItemText primary={
-                            checklist.description 
-                                ? checklist.description 
-                                : `Чек-лист: ${checklist.model_id}`
-                        } />
-                    </ListItem>
-                ))}
-            </List>
-        </Container>
-    );
+  const handleSelectChecklist = (checklistId) => {
+    dispatch(setChecklistId(checklistId));
+    navigate(`/questions`);
+  };
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
+  return (
+    <Container>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        sx={{ mt: 2, mb: 2 }}
+      >
+        <Typography variant="h5" gutterBottom>
+          Модели оценки
+        </Typography>
+        <IconButton color="primary" onClick={handleGoHome}>
+          <HomeIcon />
+        </IconButton>
+      </Box>
+
+      <List>
+        {checklists.map((checklist, index) => (
+          <ListItem
+            button
+            key={index}
+            onClick={() => handleSelectChecklist(checklist.model_id)}
+          >
+            <ListItemText
+              primary={
+                checklist.description
+                  ? checklist.description
+                  : `Чек-лист: ${checklist.model_id}`
+              }
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Container>
+  );
 };
 
 export default ChecklistList;
--- src/components/ObjectsList.js
+++ src/components/ObjectsList.js
@@ -1,9 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { List, ListItem, ListItemText, Typography, Container, CircularProgress } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { setObjectId } from '../store';
-import { getObjects } from '../api';
+import React, { useEffect, useState } from "react";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  Container,
+  CircularProgress,
+  Box,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setObjectId } from "../store";
+import { getObjects } from "../api";
 
 const ObjectsList = () => {
   const [objects, setObjects] = useState([]);
@@ -38,12 +46,18 @@ const ObjectsList = () => {
 
   return (
     <Container>
-      <Typography variant="h4" gutterBottom>
-        Objects List
-      </Typography>
+      <Box display="flex" alignItems="center" sx={{ mt: 2, mb: 2 }}>
+        <Typography variant="h5" gutterBottom>
+          Подразделения
+        </Typography>
+      </Box>
       <List>
         {objects.map((obj, index) => (
-          <ListItem button key={index} onClick={() => handleSelectObject(obj.uid)}>
+          <ListItem
+            button
+            key={index}
+            onClick={() => handleSelectObject(obj.uid)}
+          >
             <ListItemText primary={obj.name} />
           </ListItem>
         ))}
