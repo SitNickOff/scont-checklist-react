686fc5f31924eae6ccb6f0b2c10d687d1076e208
Andrey Sitnikov <a.v.sitnikov@gmail.com>
2024-09-19 19:38:14 +0300

    Убирает camera

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -144,7 +144,7 @@ const QuestionForm = ({
           multiple
           onChange={handleFilesChange}
           accept="image/*" // Ограничение на изображения
-          capture="camera" // Открытие камеры по умолчанию
+          // capture="environment" // Открытие камеры по умолчанию
         />
       </Button>
       {answer.photos && answer.photos.length > 0 && (
