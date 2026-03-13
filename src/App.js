3ea8f3d223d21d1c245fe350f6f48f934db2c1eb
Andrey Sitnikov <a.v.sitnikov@gmail.com>
2026-03-13 19:41:58 +0300

    feat: сохранение параметров и состояния при перезагрузке страницы
    - Параметры chat_id, token, lang сохраняются в sessionStorage и восстанавливаются из URL/sessionStorage/Redux
    - Состояние Redux (app slice) сохраняется в localStorage и восстанавливается при инициализации
    - isAuthorized учитывает восстановленное состояние из store

--- src/App.js
+++ src/App.js
@@ -1,29 +1,71 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { setChatId, setLang, setToken } from './store';
+import { useDispatch, useSelector } from 'react-redux';
+import store, { setChatId, setLang, setToken } from './store';
 import ObjectsList from './components/ObjectsList';
 import ChecklistList from './components/ChecklistList';
 import QuestionsStepper from './components/QuestionsStepper';
 import ThemeProviderWrapper from './ThemeProviderWrapper';
 
+const STORAGE_KEYS = {
+  CHAT_ID: 'tg_chat_id',
+  TOKEN: 'tg_token',
+  LANG: 'tg_lang',
+};
+
+const getStoredParams = () => {
+  const chatId = sessionStorage.getItem(STORAGE_KEYS.CHAT_ID);
+  const token = sessionStorage.getItem(STORAGE_KEYS.TOKEN);
+  const lang = sessionStorage.getItem(STORAGE_KEYS.LANG) || 'ru';
+  return { chatId, token, lang };
+};
+
+const storeParams = (chatId, token, lang) => {
+  sessionStorage.setItem(STORAGE_KEYS.CHAT_ID, chatId);
+  sessionStorage.setItem(STORAGE_KEYS.TOKEN, token);
+  sessionStorage.setItem(STORAGE_KEYS.LANG, lang);
+};
+
 const App = () => {
-  const [isAuthorized, setIsAuthorized] = useState(false);
+  const { chatId: storeChatId, token: storeToken } = useSelector((state) => state.app);
+  const [isAuthorized, setIsAuthorized] = useState(Boolean(storeChatId && storeToken));
   const [darkMode, setDarkMode] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
-    const chatIdParam = params.get('chat_id');
-    const tokenParam = params.get('token');
-    const langParam = params.get('lang') || 'ru';
-    
+    let chatIdParam = params.get('chat_id');
+    let tokenParam = params.get('token');
+    let langParam = params.get('lang') || 'ru';
+
+    if (!chatIdParam || !tokenParam) {
+      const stored = getStoredParams();
+      const appState = store.getState().app;
+      if (stored.chatId && stored.token) {
+        chatIdParam = stored.chatId;
+        tokenParam = stored.token;
+        langParam = stored.lang;
+      } else if (appState.chatId && appState.token) {
+        chatIdParam = appState.chatId;
+        tokenParam = appState.token;
+        langParam = appState.lang || 'ru';
+      }
+      if (chatIdParam && tokenParam) {
+        const newSearch = new URLSearchParams({
+          chat_id: chatIdParam,
+          token: tokenParam,
+          lang: langParam,
+        }).toString();
+        window.history.replaceState(null, '', `${window.location.pathname}${window.location.hash}?${newSearch}`);
+      }
+    }
+
     if (chatIdParam && tokenParam) {
+      storeParams(chatIdParam, tokenParam, langParam);
       dispatch(setChatId(chatIdParam));
       dispatch(setToken(tokenParam));
       dispatch(setLang(langParam));
       setIsAuthorized(true);
-      
     }
 
     const tg = window.Telegram.WebApp;
